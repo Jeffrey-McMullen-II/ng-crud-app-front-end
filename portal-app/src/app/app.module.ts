5aaacd28f94c1d0d8c79986935314962e96317bf
Jeffrey McMullen <jeffrey.mcmullen@Trinity.Transport>
2019-07-01 17:37:54 -0400

    Fix app component testing

--- portal-app/src/app/app.module.ts
+++ portal-app/src/app/app.module.ts
@@ -1,14 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { AppRoutingModule } from './app.routing.module';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
 
-import { AppComponent } from './app.component';
-import { UserComponent } from './user/user.component';
-import { AddUserComponent } from './user/add-user/add-user.component';
-import { UpdateUserComponent } from './user/update-user/update-user.component';
-import { UserService } from './user/shared/services/user.service';
+import {AppComponent} from './app.component';
+import {UserComponent} from './user/user.component';
+import {AddUserComponent} from './user/add-user/add-user.component';
+import {UpdateUserComponent} from './user/update-user/update-user.component';
+import {UserService} from './user/shared/services/user.service';
+import {RouterModule} from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -19,9 +19,13 @@ import { UserService } from './user/shared/services/user.service';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+      { path: 'users', component: UserComponent },
+      { path: 'add', component: AddUserComponent },
+      { path: 'update', component: UpdateUserComponent }
+    ])
   ],
   providers: [UserService],
   bootstrap: [AppComponent]
