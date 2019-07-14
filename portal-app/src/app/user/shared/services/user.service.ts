ba0e9aa3cb5f1e0f24fd1d303bda10f0153addb1
Jeffrey <jeffrey.mcmullen@yahoo.com>
2019-07-14 12:45:55 -0400

    Change headers to send application/json

--- portal-app/src/app/user/shared/services/user.service.ts
+++ portal-app/src/app/user/shared/services/user.service.ts
@@ -1,8 +1,12 @@
 import {Injectable} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 
 import {User} from '../models/user';
 import {Observable} from 'rxjs/Observable';
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
 
 @Injectable()
 export class UserService {
@@ -15,7 +19,7 @@ export class UserService {
   private userUrl = `http://localhost:8080/user-portal/users`;
 
   public createUser(user): Observable<User> {
-    return this.http.post<User>(`${this.userUrl}`, user);
+    return this.http.post<User>(`${this.userUrl}`, JSON.stringify(user), {headers: httpOptions.headers});
   }
 
   public findAllUsers(): Observable<User[]> {
@@ -27,7 +31,7 @@ export class UserService {
   }
 
   public updateUser(user): Observable<User> {
-    return this.http.put<User>(`${this.userUrl}`, user);
+    return this.http.put<User>(`${this.userUrl}`, JSON.stringify(user), {headers: httpOptions.headers});
   }
 
   public deleteUser(id: Number): Observable<User> {
