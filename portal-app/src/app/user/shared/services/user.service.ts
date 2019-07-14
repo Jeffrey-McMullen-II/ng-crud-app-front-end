48995f96a9a614ab46f93e39901909866be5f9c2
Jeffrey <jeffrey.mcmullen@yahoo.com>
2019-07-14 12:59:23 -0400

    Send http headers as application/json

--- portal-app/src/app/user/shared/services/user.service.ts
+++ portal-app/src/app/user/shared/services/user.service.ts
@@ -1,8 +1,8 @@
 import {Injectable} from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Observable} from 'rxjs/Observable';
 
 import {User} from '../models/user';
-import {Observable} from 'rxjs/Observable';
 
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
