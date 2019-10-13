322e24c216c6eddb53cd352dc794b1eb00421fc3
Jeffrey <jeffrey.mcmullen@yahoo.com>
2019-10-13 15:11:10 -0400

    Update

--- user-portal/src/main/java/com/devglan/userportal/user/UserRepository.java
+++ user-portal/src/main/java/com/devglan/userportal/user/UserRepository.java
@@ -1,6 +1,7 @@
 package com.devglan.userportal.user;
 
 import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.Repository;
 
 import java.util.List;
