06fccc50da0005b46d137f5a730ba41171d10391
Jeffrey <jeffrey.mcmullen@yahoo.com>
2020-02-23 20:03:06 -0500

    Change user repository into JpaRepository

--- back-end/src/main/java/com/crud/app/user/UserRepository.java
+++ back-end/src/main/java/com/crud/app/user/UserRepository.java
@@ -1,11 +1,13 @@
 package com.crud.app.user;
 
 import org.springframework.data.domain.Sort;
-import org.springframework.data.repository.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-public interface UserRepository extends Repository<User, Integer> {
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
 
     User save(User user);
 
